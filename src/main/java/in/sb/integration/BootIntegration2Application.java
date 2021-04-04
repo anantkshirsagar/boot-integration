adbb3816ece2c559e85e3aceef79fcbf584e3791
anantkshirsagar <40562120+anantkshirsagar@users.noreply.github.com>
2021-04-04 12:09:04 +0530

    Source

---
+++ src/main/java/in/sb/integration/BootIntegration2Application.java
@@ -0,0 +1,13 @@
+package in.sb.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIntegration2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootIntegration2Application.class, args);
+	}
+
+}
